a1647deb7233d17e6b94d1a5c4b7ae5631e82028
mannyaalonso <mannyaalonso@gmail.com>
2023-02-08 12:57:20 -0500

    update to db/index.js

--- db/index.js
+++ db/index.js
@@ -1,1 +1,14 @@
+const mongoose = require("mongoose")
 
+mongoose
+  .connect("mongodb://127.0.0.1:27017/assesmentDatabase") // if we don't have a local database named "productsDatabase" one will be created upon a successful connection
+  .then(() => {
+    console.log("Successfully connected to MongoDB.")
+  })
+  .catch((e) => {
+    console.error("Connection error", e.message)
+  })
+// mongoose.set('debug', true)
+const db = mongoose.connection
+
+module.exports = db
