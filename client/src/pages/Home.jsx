6843808db929d0dc9dda595c0b365c1780b9975e
mannyaalonso <mannyaalonso@gmail.com>
2023-02-08 21:55:55 -0500

    frontend updates

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Home = () => {
+
+
+  
+
+  return (
+    <div>
+      Home
+    </div>
+  )
+}
+
+export default Home
