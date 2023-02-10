949ec46647445e48b1b4829ef3bfaa6c52468f8e
mannyaalonso <mannyaalonso@gmail.com>
2023-02-10 14:21:45 -0500

    quiz css updates

--- client/src/pages/Assessment/Assessment.jsx
+++ client/src/pages/Assessment/Assessment.jsx
@@ -5,6 +5,8 @@ import axios from "axios"
 
 const Assessment = () => {
   const [assessment, setAssessment] = useState()
+  let userAnswers = []
+  let answers = []
 
   let { id } = useParams()
 
@@ -24,24 +26,63 @@ const Assessment = () => {
     getAssessmentById()
   }, [])
 
+  function onChange(choice, answer, index) {
+    userAnswers[index] = choice
+    answers[index] = answer
+  }
+
+  function handleExit() {
+
+  }
+
+  function handleSubmit() {
+    for (let i = 0; i < assessment.questions.length; i++) {
+      if (userAnswers[i] === null || userAnswers.length !== assessment.questions.length) {
+        
+      }
+    }
+  }
+
   return (
     assessment && (
-      <div className="assess-container">
-        {assessment.questions.map((questions) => (
-          <div key={questions.prompt} className="quiz-card">
-            <div className="quiz-question">
-              <h3>{questions.prompt}</h3>
-            </div>
-            <div className="choices">
-              {questions.choices.map((choice) => (
-                <div key={choice} className="alignment">
-                  <input id={questions.prompt} type="radio" name={questions.prompt} value={choice}/>
-                  <label for={questions.prompt}>{choice}</label>
-                </div>
-              ))}
-            </div>
+      <div>
+        <header className="quiz-header">
+          <img
+            className="img-quiz"
+            src="https://i.imgur.com/FHwiY8J.jpg"
+            alt="header"
+          />
+          <div className="quiz-titles">
+            <button onClick={handleExit} className="button-profile">Exit</button>
+            <h1 className="quiz-h1">{assessment.title} Test</h1>
+            <button onClick={handleSubmit} className="button-profile">Submit</button>
           </div>
-        ))}
+        </header>
+        <div className="quiz-container">
+          {assessment.questions.map((questions, index) => (
+            <div key={questions.prompt} className="quiz-card">
+              <div className="quiz-question">
+                <h3>{questions.prompt}</h3>
+              </div>
+              <div className="quiz-img-container">
+                {questions.codeUrl !== "" && <img className="quiz-img" src={questions.codeUrl} alt="code" />}
+              </div>
+              <div className="choices">
+                {questions.choices.map((choice) => (
+                  <div key={choice} className="alignment">
+                    <input
+                      onChange={() => onChange(choice, questions.answer, index)}
+                      name={questions.prompt}
+                      id={questions.prompt}
+                      type="radio"
+                    />
+                    <label htmlFor={questions.prompt}>{choice}</label>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     )
   )
