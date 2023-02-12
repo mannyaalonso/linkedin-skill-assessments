44c306a0df647f7c3052d0f758c41852db885bb6
mannyaalonso <mannyaalonso@gmail.com>
2023-02-12 08:03:45 -0500

    home and results css

--- client/src/pages/Results/Results.jsx
+++ client/src/pages/Results/Results.jsx
@@ -1,10 +1,19 @@
 import { useParams } from "react-router-dom"
+import "./results.css"
 
 const Results = () => {
-
   let { id } = useParams()
-  
-  return <div>{id}</div>
+
+  return (
+    <section>
+      <div className="results-container">
+        <div className="results-card">
+          <h1>Results</h1>
+            
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Results
